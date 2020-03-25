0d79269fd43f102bdbeeef6655e33c3fe0eb9fbe
Malte Rieken <malte.rieken@technia.com>
2020-03-25 15:58:33 +0100

    done transposing matrix

---
+++ ts/src/features/matrices.spec.ts
@@ -0,0 +1,110 @@
+import { compareFloat } from '../util'
+import { tuple, compare as compareTuple } from "./tuples";
+import {
+  matrix4x4,
+  matrixAt,
+  matrix2x2,
+  matrix3x3,
+  compare,
+  mul4x4,
+  transform4,
+  identity4x4,
+  transpose4x4,
+} from './matrices'
+
+test('constructing and inspecting a 4x4 matrix', () => {
+  const m = matrix4x4(
+    1,
+    2,
+    3,
+    4,
+    5.5,
+    6.5,
+    7.5,
+    8.5,
+    9,
+    10,
+    11,
+    12,
+    13.5,
+    14.5,
+    15.5,
+    16.5
+  )
+  expect(compareFloat(matrixAt(m, 0, 0), 1)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 0, 3), 4)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 1, 0), 5.5)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 1, 2), 7.5)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 2, 2), 11)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 3, 0), 13.5)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 3, 2), 15.5)).toBeTruthy()
+})
+
+test('a 2x2 matrix ought to be representable', () => {
+  const m = matrix2x2(-3, 5, 1, -2)
+  expect(compareFloat(matrixAt(m, 0, 0), -3)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 0, 1), 5)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 1, 0), 1)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 1, 1), -2)).toBeTruthy()
+})
+
+test('a 3x3 matrix ought to be representable', () => {
+  const m = matrix3x3(-3, 5, 0, 1, -2, -7, 0, 1, 1)
+  expect(compareFloat(matrixAt(m, 0, 0), -3)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 1, 1), -2)).toBeTruthy()
+  expect(compareFloat(matrixAt(m, 2, 2), 1)).toBeTruthy()
+})
+
+test('matrix equality with identical matrices', () => {
+  const a = matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2)
+  const b = matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2)
+  expect(compare(a, b)).toBeTruthy()
+})
+
+test('matrix equality with different matrices', () => {
+  const a = matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2)
+  const b = matrix4x4(2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+  expect(compare(a, b)).toBeFalsy()
+})
+
+test('multiplying two matrices', () => {
+  const a = matrix4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2)
+  const b = matrix4x4(-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8)
+  const r = matrix4x4(
+    20,
+    22,
+    50,
+    48,
+    44,
+    54,
+    114,
+    108,
+    40,
+    58,
+    110,
+    102,
+    16,
+    26,
+    46,
+    42
+  )
+  expect(compare(mul4x4(a, b), r)).toBeTruthy()
+})
+
+test('a matrix multiplied by a tuple', () => {
+  const a = matrix4x4(1, 2, 3, 4, 2, 4, 4, 2, 8, 6, 4, 1, 0, 0, 0, 1)
+  const b = tuple(1, 2, 3, 1)
+  const r = tuple(18, 24, 33, 1)
+  expect(compareTuple(transform4(a, b), r)).toBeTruthy()
+})
+
+test('multiplying a matrix by the identiy matrix', () => {
+  const a = matrix4x4(0, 1, 2, 4, 1, 2, 4, 8, 2, 4, 8, 16, 4, 8, 16, 32)
+  expect(compare(mul4x4(a, identity4x4()), a)).toBeTruthy()
+})
+
+test('transposing a matrix', () => {
+  const a = matrix4x4(0, 9, 3, 0, 9, 8, 0, 8, 1, 8, 5, 3, 0, 0, 5, 8)
+  const r = matrix4x4(0, 9, 1, 0, 9, 8, 8, 0, 3, 0, 5, 5, 0, 8, 3, 8)
+  expect(compare(transpose4x4(a), r)).toBeTruthy()
+})
---
+++ ts/src/features/matrices.ts
@@ -0,0 +1,189 @@
+import { rangeZero } from '../util'
+import { Tuple, tuple } from './tuples'
+
+/**
+ * Matrix storing elements in an array in column major order
+ */
+export interface Matrix {
+  m: number[]
+  rowSize: number
+  colSize: number
+}
+
+/**
+ * Matrix4x4 constructor in column major order
+ * @param m00
+ * @param m01
+ * @param m02
+ * @param m03
+ * @param m10
+ * @param m11
+ * @param m12
+ * @param m13
+ * @param m20
+ * @param m21
+ * @param m22
+ * @param m23
+ * @param m30
+ * @param m31
+ * @param m32
+ * @param m33
+ */
+export const matrix4x4 = (
+  m00: number,
+  m01: number,
+  m02: number,
+  m03: number,
+  m10: number,
+  m11: number,
+  m12: number,
+  m13: number,
+  m20: number,
+  m21: number,
+  m22: number,
+  m23: number,
+  m30: number,
+  m31: number,
+  m32: number,
+  m33: number
+): Matrix => {
+  return {
+    rowSize: 4,
+    colSize: 4,
+    m: [
+      m00,
+      m01,
+      m02,
+      m03,
+      m10,
+      m11,
+      m12,
+      m13,
+      m20,
+      m21,
+      m22,
+      m23,
+      m30,
+      m31,
+      m32,
+      m33,
+    ],
+  }
+}
+
+/**
+ * Matrix2x2 constructor in column major order
+ * @param m00
+ * @param m01
+ * @param m10
+ * @param m11
+ */
+export const matrix2x2 = (
+  m00: number,
+  m01: number,
+  m10: number,
+  m11: number
+): Matrix => {
+  return {
+    rowSize: 2,
+    colSize: 2,
+    m: [m00, m01, m10, m11],
+  }
+}
+
+/**
+ * Matrix3x3 constructor in column major order
+ * @param m00
+ * @param m01
+ * @param m02
+ * @param m10
+ * @param m11
+ * @param m12
+ * @param m20
+ * @param m21
+ * @param m22
+ */
+export const matrix3x3 = (
+  m00: number,
+  m01: number,
+  m02: number,
+  m10: number,
+  m11: number,
+  m12: number,
+  m20: number,
+  m21: number,
+  m22: number
+): Matrix => {
+  return {
+    rowSize: 3,
+    colSize: 3,
+    m: [m00, m01, m02, m10, m11, m12, m20, m21, m22],
+  }
+}
+
+/**
+ * Get the element of a matrix at given row and column
+ * @param m
+ * @param row
+ * @param col
+ */
+export const matrixAt = (m: Matrix, row: number, col: number): number => {
+  if (row >= m.rowSize || col >= m.colSize) return undefined
+  const i = row * m.colSize + col
+  return m.m[i]
+}
+
+export const compare = (a: Matrix, b: Matrix): boolean => {
+  if (
+    a.rowSize === b.rowSize &&
+    a.colSize === b.colSize &&
+    a.m.length == b.m.length
+  ) {
+    for (let i = 0; i < a.m.length; i++) {
+      if (a.m[i] !== b.m[i]) return false
+    }
+    return true
+  }
+
+  return false
+}
+
+export const mul4x4 = (a: Matrix, b: Matrix): Matrix => {
+  let m = matrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
+  rangeZero(4).forEach(row => {
+    rangeZero(4).forEach(col => {
+      const i = row * a.rowSize + col
+      m.m[i] =
+        matrixAt(a, row, 0) * matrixAt(b, 0, col) +
+        matrixAt(a, row, 1) * matrixAt(b, 1, col) +
+        matrixAt(a, row, 2) * matrixAt(b, 2, col) +
+        matrixAt(a, row, 3) * matrixAt(b, 3, col)
+    })
+  })
+  return m
+}
+
+export const transform4 = (a: Matrix, b: Tuple): Tuple => {
+  let t = [0, 0, 0, 0]
+  rangeZero(4).forEach(row => {
+    t[row] =
+      matrixAt(a, row, 0) * b.x +
+      matrixAt(a, row, 1) * b.y +
+      matrixAt(a, row, 2) * b.z +
+      matrixAt(a, row, 3) * b.w
+  })
+  return tuple(t[0], t[1], t[2], t[3])
+}
+
+export const identity4x4 = (): Matrix => matrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
+
+export const transpose4x4 = (m: Matrix): Matrix => {
+  const r = identity4x4()
+  rangeZero(4).forEach(row => {
+    rangeZero(4).forEach(col => {
+      const i = row * m.rowSize + col
+      r.m[i] = matrixAt(m, col, row)
+    })
+  })
+  return r
+}
